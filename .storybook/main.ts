3b5ee9c4ed050b6021cc9dc284760144391b60a5
joedysonbezerra <joedysonbezerra@gmail.com>
2025-01-18 18:50:57 -0300

    chore: update Storybook configuration to include specific Colors.mdx story and ensure proper addon formatting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,12 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/Colors.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/nextjs',
