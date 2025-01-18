e01cc427c6ace7d1fe94c519dda857f539252735
joedysonbezerra <joedysonbezerra@gmail.com>
2025-01-17 22:31:09 -0300

    refactor: update article import paths to remove locale-specific directory structure

--- src/app/shared/@JustLaunch/services/blog/articles.ts
+++ src/app/shared/@JustLaunch/services/blog/articles.ts
@@ -5,7 +5,7 @@ async function importArticle(
   articleFilename: string
 ): Promise<IArticleWithSlug> {
   const { article } = await import(
-    `../../../../../app/[locale]/articles/${articleFilename.replace('page.mdx', 'metadata')}`
+    `../../../../../app/articles/${articleFilename.replace('page.mdx', 'metadata')}`
   );
 
   return {
@@ -16,7 +16,7 @@ async function importArticle(
 
 export async function getAllArticles() {
   let articleFilenames = await glob('*/page.mdx', {
-    cwd: './src/app/[locale]/articles',
+    cwd: './src/app/articles',
   });
   console.log(articleFilenames[0]);
 
